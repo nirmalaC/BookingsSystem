d0bffc64d300741b9b08bfe19090e498555325ad
nchaliki <nirmala.chaliki@hermes-europe.co.uk>
2021-02-03 20:32:21 +0000

    Refactored the global wait

--- src/test/java/helpers/UiHelper.java
+++ src/test/java/helpers/UiHelper.java
@@ -9,12 +9,13 @@ import java.util.logging.Logger;
 
 public class UiHelper {
 
+   static  WebDriverWait wait = new WebDriverWait(Hooks.driver, 10);
+
     /**
-     *
+     * This Explicit Wait method is to handle the wait for elements to be visible on the page
      * @param element This is the webelement that has to be visible on the web page.
      */
     public static void waitForVisibleElement(WebElement element){
-        WebDriverWait wait = new WebDriverWait(Hooks.driver, 30);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
@@ -37,9 +38,10 @@ public class UiHelper {
         }
     }
 
-
+    /**
+     * This JavascriptExecutor method is to handle the ajax wait
+     */
     public static void waitForJQueryLoad() {
-        WebDriverWait wait = new WebDriverWait(Hooks.driver, 10);
         try {
             JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Hooks.driver;
             ExpectedCondition<Boolean> jQueryLoad = driver -> ((Long) ((JavascriptExecutor) Hooks.driver)
